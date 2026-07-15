b12cb58c3094d9f1e2529d829eb1561321268b16
Parinav <parinav622@gmail.com>
2026-07-15 19:53:38 +0530

    Calculate simple interest.
    enter principle amount, interest% per year, time(in yrs) and it will generate simple interest.

---
+++ simple interest.py
@@ -0,0 +1,5 @@
+prin=int(input("enter principle amount:"))
+interest=int(input("enter interest% per year:"))
+time=int(input("enter time(in years):"))
+SI=(prin*interest*time)/100
+print("Simple interest:",SI)
