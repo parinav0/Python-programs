3d0f270041b6c9f7fd6a4504fb909b12f3776f32
Parinav <parinav622@gmail.com>
2026-07-17 23:03:04 +0530

    Verify you're a human
    A simple verification thing like CAPTCHA.

---
+++ Verify you're not a bot.py
@@ -0,0 +1,12 @@
+print("Verify youre a human:-")
+print("wmdkem1mdew0jsdjkkk2kjnk")
+n=102
+a=int(input("Enter the numbers in the above line:"))
+if(a==n):
+    print("Verified")
+while(a!=n):
+  if(a!=n):
+        a=int(input("Not Verified, try again:"))
+  if(a==n):
+        print("Verified")
+        
