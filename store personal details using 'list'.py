ff229eca77005c017d6c588ad2d59df040826ab2
Parinav <parinav622@gmail.com>
2026-07-24 23:07:48 +0530

    Store personal details using 'list'

---
+++ store personal details using 'list'.py
@@ -0,0 +1,8 @@
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+nationality=input("Nationality:")
+edu=input("Enter your educational qualifications:")
+lang=input("Native langage:")
+status=input("Enter status(employed or unemployed or student):")
+list1=[name,age,nationality,edu,lang,status]
+print(list1)
