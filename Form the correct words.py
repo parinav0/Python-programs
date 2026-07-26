37630206ec0afc26a254d2234e1cebc05f4a812f
Parinav <parinav622@gmail.com>
2026-07-26 23:16:33 +0530

    Exercise-Form the correct words
    The questions are asked using 'list'. ex: Q)['h','e','l','l'). then it asks what should be the last letter.  You can enter 'o' and it prints the result:['h','e','l','l','o'] and asks other questions after this.

---
+++ Form the correct words.py
@@ -0,0 +1,47 @@
+list1=['n','e']
+print("1 - ",list1)
+value=input("enter what letter comes at end of the list:")
+while(value!="w"):
+        value=input("word does not match,try again:")
+print("correct")
+list1.append(value)
+print("result - ",list1)
+
+list2=['F','u','r','t','i','v','e','l']
+print("2 - ",list2)
+value=input("enter what letter comes at end of the list:")
+while(value!="y"):
+        value=input("word does not match,try again:")
+print("correct")
+list2.append(value)
+print("result - ",list2)
+
+list3=['s','t','i']
+print("3 - ",list3)
+value=input("enter what letter comes at end of the list:")
+while(value!="r"):
+        value=input("word does not match,try again:")
+print("correct")
+list3.append(value)
+print("result - ",list3)
+
+list4=['s','l']
+print("4 - ",list4)
+value=input("enter what letter comes at end of the list:")
+while(value!="y"):
+        value=input("word does not match,try again:")
+print("correct")
+list4.append(value)
+print("result - ",list4)
+
+list5=['G','r','a','s']
+print("5 - ",list5)
+value=input("enter what letter comes at end of the list:")
+while(value!="p" and value!="s"):
+        value=input("word does not match,try again:")
+print("correct")
+list5.append(value)
+print("result - ",list5)
+
+print("The end")
+
